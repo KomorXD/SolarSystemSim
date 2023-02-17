fbd74142112615a1218ff632f94fd7b35e8c0b8e
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-02-17 14:23:10 +0100

    Now Scene is responsible for providing it's config

--- src/scenes/Scene.hpp
+++ src/scenes/Scene.hpp
@@ -11,6 +11,7 @@ public:
 	virtual void OnInput()			= 0;
 	virtual void OnUpdate(float ts)	= 0;
 	virtual void OnRender()			= 0;
+	virtual void OnConfigRender()	= 0;
 
 	virtual uint32_t GetFramebufferTextureID() const = 0;
 };
