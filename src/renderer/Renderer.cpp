4d49005d63a32167f4621ee453b53a5d3b3e0384
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-02-18 22:25:06 +0100

    Add structure for batch rendering primitives
    BUG: for some reason rendering more than 1 sphere doesn't work will check tomorrow

--- src/renderer/Renderer.cpp
+++ src/renderer/Renderer.cpp
@@ -1,6 +1,9 @@
 #include "Renderer.hpp"
 #include "../OpenGL.hpp"
 #include "../Logger.hpp"
+#include "../Timer.hpp"
+
+#include <glm/gtc/matrix_transform.hpp>
 
 static void OpenGLMessageCallback(
 	unsigned source,
@@ -23,8 +26,131 @@ static void OpenGLMessageCallback(
 
 glm::mat4 Renderer::s_ViewProjection = glm::mat4(1.0f);
 
+struct SphereVertex
+{
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec4 Color;
+};
+
+struct LineVertex
+{
+	glm::vec3 Position;
+	glm::vec4 Color;
+};
+
+struct BasicVertex
+{
+	glm::vec4 Position;
+	glm::vec3 Normal;
+};
+
+struct RendererData
+{
+	static constexpr uint32_t MaxQuads		   = 10000;
+	static constexpr uint32_t MaxVertices	   = MaxQuads * 4;
+	static constexpr uint32_t MaxIndices	   = MaxQuads * 6;
+	static constexpr uint32_t IndicesPerSphere = 5952;
+
+	std::shared_ptr<VertexArray>  SphereVertexArray;
+	std::shared_ptr<VertexBuffer> SphereVertexBuffer;
+	std::shared_ptr<Shader>		  SphereShader;
+
+	uint32_t SphereIndexCount	   = 0;
+	SphereVertex* SphereBufferBase = nullptr;
+	SphereVertex* SphereBufferPtr  = nullptr;
+
+	std::vector<BasicVertex> SphereVertices;
+};
+
+static RendererData s_Data{};
+
+std::vector<BasicVertex> GenerateUVSphereVertices(int32_t sectorCount, int32_t stackCount)
+{
+	constexpr float PI = 3.14159265358979323846f;
+	constexpr float radius = 1.0f;
+
+	std::vector<BasicVertex> vertices{};
+
+	float lengthInv = 1.0f / radius;
+	float sectorStep = 2.0f * PI / sectorCount;
+	float stackStep = PI / stackCount;
+
+	for (int32_t i = 0; i <= stackCount; ++i)
+	{
+		float stackAngle = PI / 2.0f - i * stackStep;
+
+		float xy = radius * std::cosf(stackAngle);
+		float z = radius * std::sinf(stackAngle);
+
+		for (int32_t j = 0; j <= sectorCount; ++j)
+		{
+			float sectorAngle = j * sectorStep;
+
+			float x = xy * std::cosf(sectorAngle);
+			float y = xy * std::sinf(sectorAngle);
+
+			float nx = x * lengthInv;
+			float ny = y * lengthInv;
+			float nz = z * lengthInv;
+
+			BasicVertex v;
+
+			v.Position = glm::vec4(x, y, z, 1.0f);
+			v.Normal   = glm::vec3(nx, ny, nz);
+
+			vertices.push_back(v);
+		}
+	}
+
+	return vertices;
+}
+
+std::vector<uint32_t> GenerateUVSphereIndices(int32_t sectorCount, int32_t stackCount, int32_t maxIndices)
+{
+	int32_t offset = 0;
+
+	std::vector<uint32_t> indices{};
+	int32_t k1{};
+	int32_t k2{};
+
+	indices.reserve(maxIndices);
+
+	while (offset + s_Data.IndicesPerSphere < maxIndices)
+	{
+		for (int32_t i = 0; i < stackCount; ++i)
+		{
+			k1 = i * (sectorCount + 1) + offset;
+			k2 = k1 + sectorCount + 1;
+
+			for (int32_t j = 0; j < sectorCount; ++j, ++k1, ++k2)
+			{
+				if (i != 0)
+				{
+					indices.push_back(k1);
+					indices.push_back(k2);
+					indices.push_back(k1 + 1);
+				}
+
+				if (i != (stackCount - 1))
+				{
+					indices.push_back(k1 + 1);
+					indices.push_back(k2);
+					indices.push_back(k2 + 1);
+				}
+			}
+		}
+		
+		offset += s_Data.IndicesPerSphere;
+	}
+
+	return indices;
+}
+
 void Renderer::Init()
 {
+	FUNC_PROFILE();
+
 #ifdef CONF_DEBUG
 	GLCall(glEnable(GL_DEBUG_OUTPUT));
 	GLCall(glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS));
@@ -36,7 +162,36 @@ void Renderer::Init()
 	GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
 	GLCall(glEnable(GL_DEPTH_TEST));
+	GLCall(glDepthFunc(GL_LESS));
+	
+	GLCall(glEnable(GL_CULL_FACE));
 	GLCall(glEnable(GL_LINE_SMOOTH));
+
+	s_Data.SphereVertexArray = std::make_shared<VertexArray>();
+	s_Data.SphereVertexBuffer = std::make_shared<VertexBuffer>(nullptr, s_Data.MaxVertices * sizeof(SphereVertex));
+
+	VertexBufferLayout layout;
+
+	layout.Push<float>(3);
+	layout.Push<float>(3);
+	layout.Push<float>(4);
+
+	std::vector<uint32_t> sphereIndices = GenerateUVSphereIndices(32, 32, s_Data.MaxIndices);
+
+	std::unique_ptr<IndexBuffer> ibo = std::make_unique<IndexBuffer>(sphereIndices.data(), sphereIndices.size());
+
+	s_Data.SphereVertexArray->AddBuffer(*s_Data.SphereVertexBuffer, ibo, layout);
+	s_Data.SphereVertexArray->Unbind();
+
+	s_Data.SphereBufferBase = new SphereVertex[s_Data.MaxVertices];
+	s_Data.SphereShader = std::make_shared<Shader>("res/shaders/Sphere.vert", "res/shaders/Sphere.frag");
+
+	s_Data.SphereVertices = GenerateUVSphereVertices(32, 32);
+}
+
+void Renderer::Shutdown()
+{
+	delete[] s_Data.SphereBufferBase;
 }
 
 void Renderer::OnWindowResize(uint32_t width, uint32_t height)
@@ -47,10 +202,26 @@ void Renderer::OnWindowResize(uint32_t width, uint32_t height)
 void Renderer::SceneBegin(Camera& camera)
 {
 	s_ViewProjection = camera.GetViewProjection();
+
+	StartBatch();
 }
 
 void Renderer::SceneEnd()
 {
+	Flush();
+}
+
+void Renderer::Flush()
+{
+	if (s_Data.SphereIndexCount)
+	{
+		uint32_t dataSize = (uint32_t)((uint8_t*)s_Data.SphereBufferPtr - (uint8_t*)s_Data.SphereBufferBase);
+
+		s_Data.SphereVertexBuffer->SetData(s_Data.SphereBufferBase, dataSize);
+		s_Data.SphereShader->Bind();
+
+		DrawIndexed(s_Data.SphereShader, s_Data.SphereVertexArray, s_Data.SphereIndexCount);
+	}
 }
 
 void Renderer::ClearColor(const glm::vec4& color)
@@ -63,6 +234,39 @@ void Renderer::Clear()
 	GLCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 }
 
+void Renderer::DrawSphere(const glm::vec3& position, float radius, const glm::vec4& color)
+{
+	if (s_Data.SphereIndexCount + s_Data.IndicesPerSphere >= s_Data.MaxIndices)
+	{
+		NextBatch();
+	}
+
+	glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), glm::vec3(radius));
+
+	for (size_t i = 0; i < s_Data.SphereVertices.size(); ++i)
+	{
+		s_Data.SphereBufferPtr->Position = transform * s_Data.SphereVertices[i].Position;
+		s_Data.SphereBufferPtr->Normal = s_Data.SphereVertices[i].Normal;
+		s_Data.SphereBufferPtr->Color = color;
+
+		++s_Data.SphereBufferPtr;
+	}
+
+	s_Data.SphereIndexCount += s_Data.IndicesPerSphere;
+}
+
+void Renderer::DrawIndexed(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vao, uint32_t count)
+{
+	uint32_t indices = count ? count : vao->GetIndexBuffer()->GetCount();
+
+	shader->Bind();
+	shader->SetUniformMat4("u_ViewProjection", s_ViewProjection);
+
+	vao->Bind();
+
+	GLCall(glDrawElements(GL_TRIANGLES, indices, GL_UNSIGNED_INT, nullptr));
+}
+
 void Renderer::SubmitIndexed(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vao, const glm::mat4& transform)
 {
 	shader->Bind();
@@ -72,4 +276,25 @@ void Renderer::SubmitIndexed(const std::shared_ptr<Shader>& shader, const std::s
 	vao->Bind();
 
 	GLCall(glDrawElements(GL_TRIANGLES, vao->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr));
+}
+
+void Renderer::ToggleWireframe()
+{
+	GLint mode[2]{};
+
+	GLCall(glGetIntegerv(GL_POLYGON_MODE, mode));
+	mode[0] = (mode[0] == GL_LINE ? GL_FILL : GL_LINE);
+	GLCall(glPolygonMode(GL_FRONT_AND_BACK, mode[0]));
+}
+
+void Renderer::StartBatch()
+{
+	s_Data.SphereIndexCount = 0;
+	s_Data.SphereBufferPtr = s_Data.SphereBufferBase;
+}
+
+void Renderer::NextBatch()
+{
+	Flush();
+	StartBatch();
 }
--- src/renderer/Renderer.hpp
+++ src/renderer/Renderer.hpp
@@ -12,17 +12,26 @@ class Renderer
 {
 public:
 	static void Init();
+	static void Shutdown();
 
 	static void OnWindowResize(uint32_t width, uint32_t height);
 
 	static void SceneBegin(Camera& camera);
 	static void SceneEnd();
+	static void Flush();
 
 	static void ClearColor(const glm::vec4& color);
 	static void Clear();
 
+	static void DrawSphere(const glm::vec3& position, float radius, const glm::vec4& color);
+	static void DrawIndexed(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vao, uint32_t count = 0);
 	static void SubmitIndexed(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vao, const glm::mat4& transform = glm::mat4(1.0f));
 
+	static void ToggleWireframe();
+
 private:
+	static void StartBatch();
+	static void NextBatch();
+
 	static glm::mat4 s_ViewProjection;
 };
