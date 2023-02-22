af7600bc1cf1f2c6953a577cfec97fc1f6750c31
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-02-22 15:31:47 +0100

    Add an interface for scene states

--- src/scenes/states/SceneState.hpp
+++ src/scenes/states/SceneState.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
 struct Event;
+class Camera;
 
 class SceneState
 {
 public:
-	virtual void OnEvent(Event& ev) = 0;
-	virtual void OnUpdate(float ts) = 0;
-	virtual void OnRender()			= 0;
+	virtual void OnEvent(Event& ev)		  = 0;
+	virtual void OnUpdate(float ts)		  = 0;
+	virtual void OnRender(Camera& camera) = 0;
+	virtual void OnConfigRender()		  = 0;
 };
