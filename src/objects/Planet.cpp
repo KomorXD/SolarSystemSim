b8300d4c8a0828731d12ca682dbe125befc08502
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-02-23 17:32:47 +0100

    Make planet obj more flexible

--- src/objects/Planet.cpp
+++ src/objects/Planet.cpp
@@ -22,6 +22,23 @@ void Planet::SetPosition(const glm::vec3& position)
 	m_Position = position;
 }
 
+void Planet::SetScale(const glm::vec3& scale)
+{
+	m_Scale = scale;
+	m_Radius = glm::max(glm::max(scale.x, scale.y), scale.z);
+}
+
+void Planet::SetRadius(float radius)
+{
+	m_Radius = radius;
+	m_Scale = glm::vec3(radius);
+}
+
+void Planet::SetRotation(const glm::vec3& rotation)
+{
+	m_Rotation = rotation;
+}
+
 void Planet::SetColor(const glm::vec4& color)
 {
 	m_Color = color;
@@ -32,7 +49,8 @@ void Planet::OnConfigRender()
 	ImGui::BeginChild("Sphere settings");
 	ImGui::Text("Planet #%d", m_EntityID);
 	ImGui::DragFloat3("Position", glm::value_ptr(m_Position), 0.01f, -FLT_MAX, FLT_MAX, "%.2f");
+	ImGui::DragFloat3("Rotation", glm::value_ptr(m_Rotation), 0.01f, -FLT_MAX, FLT_MAX, "%.2f");
+	ImGui::DragFloat3("Scale",	  glm::value_ptr(m_Scale),    0.01f, -FLT_MAX, FLT_MAX, "%.2f");
 	ImGui::ColorEdit4("Color", glm::value_ptr(m_Color));
-	ImGui::DragFloat("Radius", &m_Radius, 0.01f, 0.0f, 10.0f, "%.2f");
 	ImGui::EndChild();
 }
--- src/objects/Planet.hpp
+++ src/objects/Planet.hpp
@@ -3,31 +3,44 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/quaternion.hpp>
+
 class Planet
 {
 public:
 	Planet(const glm::vec3& position);
 
 	void Move(const glm::vec3& offset);
+	
 	void SetPosition(const glm::vec3& position);
+	void SetScale(const glm::vec3& scale);
+	void SetRadius(float radius);
+	void SetRotation(const glm::vec3& rotation);
 	void SetColor(const glm::vec4& color);
 
 	void OnConfigRender();
 
 	inline glm::vec3 GetPosition() const { return m_Position; }
+	inline glm::vec3 GetRotation() const { return m_Rotation; }
+	inline glm::vec3 GetScale()	   const { return m_Scale;	  }
 	inline glm::vec4 GetColor()	   const { return m_Color;    }
 	inline float GetRadius()	   const { return m_Radius;   }
 	inline uint32_t GetEntityID()  const { return m_EntityID; }
 
 	inline glm::mat4 GetTransform() const
 	{
-		return glm::translate(glm::mat4(1.0f), m_Position) * glm::scale(glm::mat4(1.0f), glm::vec3(m_Radius));
+		return glm::translate(glm::mat4(1.0f), m_Position) 
+			* glm::scale(glm::mat4(1.0f), m_Scale)
+			* glm::toMat4(glm::quat(m_Rotation));
 	}
 
 	inline static constexpr uint32_t MAX_PLANETS = 254;
 
 private:
 	glm::vec3 m_Position = { 0.0f, 0.0f, 0.0f };
+	glm::vec3 m_Rotation = { 0.0f, 0.0f, 0.0f };
+	glm::vec3 m_Scale	 = { 1.0f, 1.0f, 1.0f };
 	glm::vec4 m_Color	 = { 0.2f, 0.2f, 0.2f, 1.0f };
 
 	float m_Radius = 1.0f;
