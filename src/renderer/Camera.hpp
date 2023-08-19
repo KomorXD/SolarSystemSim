09e52ee26e00360c37d49b844ee1505998bbbdbf
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-08-19 17:15:03 +0200

    Increase far clipping plane

--- src/renderer/Camera.hpp
+++ src/renderer/Camera.hpp
@@ -51,7 +51,7 @@ private:
 	float m_FOV			= 45.0f;
 	float m_AspectRatio = 16.0f / 9.0f;
 	float m_NearClip	= 0.1f;
-	float m_FarClip		= 100.0f;
+	float m_FarClip		= 1000.0f;
 
 	glm::mat4 m_Projection = glm::mat4(1.0f);
 	glm::mat4 m_View	   = glm::mat4(1.0f);
