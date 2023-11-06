729b40d5ffe87ebf20f31a12bbe654fce51510bb
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-11-06 14:40:48 +0100

    Insert 1x1 white pixel, works so far

--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -1,8 +1,48 @@
 #include "TextureManager.hpp"
-#include "OpenGL.hpp"
+#include "stb_rect_pack/stb_rect_pack.h"
+#include "stb_image/stb_image_write.h"
+#include "Logger.hpp"
+#include "Timer.hpp"
 
+std::unique_ptr<Texture> TextureManager::s_Atlas;
 std::vector<TextureInfo> TextureManager::s_Textures;
 int32_t TextureManager::s_IndexCounter = 1;
+
+stbrp_context TextureManager::s_Context;
+std::vector<stbrp_rect> TextureManager::s_Rects;
+std::vector<stbrp_node> TextureManager::s_Nodes;
+
+void TextureManager::Init()
+{
+	FUNC_PROFILE();
+
+	s_Atlas = std::make_unique<Texture>(4096, 4096);
+	s_Nodes.resize(255);
+
+	stbrp_rect whitePixelRect{};
+	whitePixelRect.id = s_IndexCounter;
+	whitePixelRect.w = 1;
+	whitePixelRect.h = 1;
+	s_Rects.push_back(whitePixelRect);
+	s_IndexCounter++;
+
+	stbrp_init_target(&s_Context, 4096, 4096, s_Nodes.data(), s_Nodes.size());
+
+	if (stbrp_pack_rects(&s_Context, s_Rects.data(), s_Rects.size()))
+	{
+		LOG_INFO("Initialized stbrp_rect_pack.");
+	}
+	else
+	{
+		LOG_ERROR("A 1x1 pixel somehow didn't fit xd");
+	}
+
+	uint8_t whitePixelData[4] = { 255, 255, 255, 255 };
+	whitePixelRect = s_Rects[0];
+
+	s_Atlas->SetSubtexture(whitePixelData, { whitePixelRect.x, whitePixelRect.y }, { whitePixelRect.w, whitePixelRect.h });
+	s_Textures.push_back({ whitePixelRect.id, "", { whitePixelRect.x, whitePixelRect.y }, { whitePixelRect.w, whitePixelRect.h } });
+}
 
 TextureInfo TextureManager::AddTexture(const std::string& path)
 {
@@ -14,6 +54,7 @@ TextureInfo TextureManager::AddTexture(const std::string& path)
 	}
 
 	// add to atlas
+	
 
 	s_Textures.push_back({ s_IndexCounter, path });
 	s_IndexCounter++;
--- src/TextureManager.hpp
+++ src/TextureManager.hpp
@@ -4,29 +4,38 @@
 #include <optional>
 #include <vector>
 
+#include "stb_rect_pack/stb_rect_pack.h"
+#include "OpenGL.hpp"
+
 struct TextureInfo
 {
 	int32_t TextureID;
 	std::string Path;
-	// glm::vec2 UV;
-	// glm::vec2 Size;
+	glm::ivec2 UV;
+	glm::ivec2 Size;
 };
 
 class TextureManager
 {
 public:
-	TextureInfo AddTexture(const std::string& path);
+	static void Init();
 
-	std::optional<TextureInfo> GetTexture(const std::string& path);
-	std::optional<TextureInfo> GetTexture(uint32_t id);
+	static TextureInfo AddTexture(const std::string& path);
+
+	static std::optional<TextureInfo> GetTexture(const std::string& path);
+	static std::optional<TextureInfo> GetTexture(uint32_t id);
 	
-	bool RemoveTexture(const std::string& path);
-	bool RemoveTexture(uint32_t id);
+	static bool RemoveTexture(const std::string& path);
+	static bool RemoveTexture(uint32_t id);
 
 private:
 	TextureManager() = default;
 
-	// static Texture s_Atlas;
+	static std::unique_ptr<Texture> s_Atlas;
 	static std::vector<TextureInfo> s_Textures;
 	static int32_t s_IndexCounter;
+
+	static stbrp_context s_Context;
+	static std::vector<stbrp_node> s_Nodes;
+	static std::vector<stbrp_rect> s_Rects;
 };
