79dee60ae99ab3f48133e06631c4de32ea16cc05
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-11-29 18:41:57 +0100

    Upgrade to 4.3

--- src/Application.cpp
+++ src/Application.cpp
@@ -33,7 +33,7 @@ Application::Application(const WindowSpec& spec)
 
 	LOG_INFO("GLFW initialized");
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
